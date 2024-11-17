66d5075b17e60572937c46915048c7bde021b607
Patryk Wos <patrykwos05@gmail.com>
2024-11-17 20:10:58 +0000

    Revert "add practice mode"
    This reverts commit aca15c9832da08ee1277f7864a3b9705d8d24b3d.

--- src/api/dictionary_api.tsx
+++ src/api/dictionary_api.tsx
@@ -4,13 +4,9 @@ async function getWordDefinition(word: string) {
     return data;
 }
 
-function extractWordDefinition(wordDefinition: any) {
+function parseWordDefinition(wordDefinition: any) {
     const firstDefinition = wordDefinition[0].meanings[0].definitions[0].definition;
     return firstDefinition;
 }
 
-function extractMeanings(wordDefinition: any) {
-    return wordDefinition[0].meanings;
-}
-
-export { getWordDefinition, extractWordDefinition as parseWordDefinition, extractMeanings };
+export { getWordDefinition, parseWordDefinition };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Flashcard from '@/components/Flashcard';
-import PracticeFlashcard from '@/components/PracticeFlashcard';
 import words from '@/data/words.json';
 import styles from '@/styles/page.module.css';
 import { useState } from 'react';
@@ -9,8 +8,6 @@ import { useState } from 'react';
 
 export default function Home() {
     const [wordList, setWordList] = useState(words.words);
-    const [isPracticeMode, setIsPracticeMode] = useState(false);
-    const [currentWordIndex, setCurrentWordIndex] = useState(0);
 
     const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -20,39 +17,17 @@ export default function Home() {
         setWordList(newWords);
     }
 
-    const getNextWord = () => {
-        const randomIndex = Math.floor(Math.random() * wordList.length);
-        setCurrentWordIndex(randomIndex);
-    }
-
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>Flashcards</h1>
             <input name="Upload words" type="file" accept=".txt" onChange={handleFileUpload} className={styles.fileInput} />
-            <button className={styles.practiceModeButton} onClick={() => setIsPracticeMode(!isPracticeMode)}>{isPracticeMode ? 'Switch to Study Mode' : 'Switch to Practice Mode'}</button>
-            
-            {isPracticeMode ? (
-                <div className={styles.practiceContainer}>
-                    <PracticeFlashcard 
-                        word={wordList[currentWordIndex]} 
+            <div className={styles.flashcardContainer}>
+                {wordList.map((word) => (
+                    <Flashcard 
+                        word={word} 
                     />
-                    <button 
-                        onClick={getNextWord}
-                        className={styles.nextButton}
-                    >
-                        Next Card
-                    </button>
-                </div>
-            ) : (
-                <div className={styles.flashcardContainer}>
-                    {wordList.map((word) => (
-                        <Flashcard 
-                            key={word}
-                            word={word} 
-                        />
-                    ))}
-                </div>
-            )}
+                ))}
+            </div>
         </main>
     );
 }
